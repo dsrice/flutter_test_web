739c94679e8ad1bee80f440cd4346df25c30154e
dsrice <v.fuji.0223@gmail.com>
2021-02-21 22:06:45 +0900

    createsuperuserを修正

--- v1/manager.py
+++ v1/manager.py
@@ -2,14 +2,13 @@ from django.contrib.auth.base_user import BaseUserManager
 
 
 class CustomUserManager(BaseUserManager):
-    def create_user(self, email, password, **extra_fields):
-        if not email:
-            raise ValueError(_('The Email must be set'))
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+    def create_user(self, loginid, password, **extra_fields):
+        if not loginid:
+            raise ValueError(_('The loginid must be set'))
+        user = self.model(loginid=loginid, **extra_fields)
         user.set_password(password)
         user.save()
         return user
 
-    def create_superuser(self, email, password, **extra_fields):
-        return self.create_user(email, password, **extra_fields)
+    def create_superuser(self, loginid, password, **extra_fields):
+        return self.create_user(loginid, password, **extra_fields)
