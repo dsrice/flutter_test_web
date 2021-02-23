27b76e4bf0e40f6334237a3e8ead2431c74176c7
dsrice <v.fuji.0223@gmail.com>
2021-02-23 17:47:05 +0900

    お知らせ用のurlを用意

--- bell_web/urls.py
+++ bell_web/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from bell_web.views import login
+from bell_web.views import login, article
 
 urlpatterns = [
-    path("login/", login.index, name="login")
+    path("login/", login.index, name="login"),
+    path("article/", article.index, name="article_index")
 ]
