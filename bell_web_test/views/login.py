e8f1040dd7b39100d5d80e1db85c329e290061be
dsrice <v.fuji.0223@gmail.com>
2021-02-26 02:12:53 +0900

    認証周りのバックエンドを用意

--- bell_web_test/views/login.py
+++ bell_web_test/views/login.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from bell_web_test.forms.login.loginForm import LoginForm
+from django.contrib.auth import authenticate, login
+
 
 def index(request):
-    return render(request, "login/index.html")
+    """
+    認証画面
+    :param request:
+    :return:
+    """
+    form = LoginForm(request.POST or None)
+    if form.is_valid():
+        loginid = form.data["loginid"]
+        user = authenticate(loginid=loginid, password=form.data["password"])
+        if user:
+            login(request, user)
+
+    return render(request, "login/index.html", {"form": form})
